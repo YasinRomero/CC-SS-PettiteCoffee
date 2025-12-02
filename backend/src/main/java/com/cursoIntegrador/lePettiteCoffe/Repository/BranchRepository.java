95f81dccb3af80135505e03663f1c9017259464c
Frosberg <165091658+Frosberg@users.noreply.github.com>
2025-12-01 20:00:54 -0500

    Agregar Javadoc en BranchRepository.java

--- backend/src/main/java/com/cursoIntegrador/lePettiteCoffe/Repository/BranchRepository.java
+++ backend/src/main/java/com/cursoIntegrador/lePettiteCoffe/Repository/BranchRepository.java
@@ -5,6 +5,9 @@ import org.springframework.stereotype.Repository;
 
 import com.cursoIntegrador.lePettiteCoffe.Model.Entity.Branch;
 
+/**
+ * Repositorio para gestionar las operaciones CRUD de la entidad {@link Branch}.
+ */
 @Repository
 public interface BranchRepository extends JpaRepository<Branch,Integer>{
     
