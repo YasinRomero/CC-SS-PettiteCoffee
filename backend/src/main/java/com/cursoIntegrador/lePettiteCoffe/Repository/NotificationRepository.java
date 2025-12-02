4a02397fac94d424b75efc2168ab0fb39d73dfaa
Frosberg <165091658+Frosberg@users.noreply.github.com>
2025-12-01 20:01:13 -0500

    Agregar Javadoc en NotificationRepository.java

--- backend/src/main/java/com/cursoIntegrador/lePettiteCoffe/Repository/NotificationRepository.java
+++ backend/src/main/java/com/cursoIntegrador/lePettiteCoffe/Repository/NotificationRepository.java
@@ -7,7 +7,17 @@ import org.springframework.stereotype.Repository;
 
 import com.cursoIntegrador.lePettiteCoffe.Model.Entity.Notification;
 
+/**
+ * Repositorio para gestionar las operaciones de la entidad {@link Notification}.
+ */
 @Repository
 public interface NotificationRepository extends JpaRepository<Notification, Integer> {
+
+    /**
+     * Obtiene la lista de notificaciones asociadas a una cuenta mediante su email.
+     *
+     * @param email el correo electrónico de la cuenta.
+     * @return una lista de notificaciones relacionadas con la cuenta indicada.
+     */
     List<Notification> findByCuentaEmail(String email);
 }
